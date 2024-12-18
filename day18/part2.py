243362df324961ebb382566d2af27ac0f1444212
Neil Taylor <uklocalbeer@gmail.com>
2024-12-18 13:01:20 +0000

    Simple optimisation; only recalculate path when a block lands on it

--- day18/part2.py
+++ day18/part2.py
@@ -5,17 +5,18 @@ input = open("day18/input.txt", "r")
 lines = input.read().splitlines()
 
 count = 0
+path = None
 for line in lines:
     col = int(line.split(",")[0])
     row = int(line.split(",")[1])
     grid.grid[col][row] = "#"
     count += 1
 
+    if path is not None and (col, row) not in path:
+        continue
+
     if count > 1024:
         path = grid.dijkstra()
-        if len(path) > 1:
-            if count % 100 == 0:
-                print(f"OK after {count} blocks, path is {len(path)-1}")
-        else:
+        if len(path) == 1:
             print(f"No path for {col},{row}")
             break
